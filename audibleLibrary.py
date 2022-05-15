61f54d6e0bc1adcf4a535042fbcda4ebcc73258f
Patrick McGovern <scorer61@yahoo.com>
2022-05-14 20:46:18 -0400

    Added additional elements to list.

--- audibleLibrary.py
+++ audibleLibrary.py
@@ -3,6 +3,14 @@ import audible
 FILENAME = 'auth/audible_auth.txt'
 AUTH = audible.Authenticator.from_file(FILENAME)
 
+def convertTime(value):
+    try:
+        hours = str(int(value / 60))
+        minutes = str(value % 60)
+        return hours + ':' + minutes.zfill(2)
+    except:
+        return 'N/A'
+
 with audible.Client(auth=AUTH) as client:
     library = client.get(
         "1.0/library",
@@ -17,9 +25,11 @@ with audible.Client(auth=AUTH) as client:
         title = book['title']
         purchased = book['purchase_date']
         released = book['release_date']
-        runtime = book['runtime_length_min']
-        book_list.append([title,runtime,released,purchased])
+        runtime_mmm = book['runtime_length_min']
+        runtime_hm = convertTime(runtime_mmm)
+        book_list.append([title,runtime_mmm,runtime_hm,released,purchased])
 
 book_list.sort()
+book_list.insert(0,['title','runtime_mmm','runtime_hm','released','purchased'])
 for book in book_list:
     print(book)
